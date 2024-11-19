4e92ed6dcafcb92b53b2ebe0faaf22dc68b20870
Pedro Leta <pedrogleta@gmail.com>
2024-11-19 20:07:33 -0300

    add ability for checking todos

--- lib/screens/todo.dart
+++ lib/screens/todo.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class TodoListScreen extends StatelessWidget {
-  TodoListScreen({super.key});
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
 
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
   final List<Map<String, String>> tasks = [
     {'task': 'Desligar as luzes que não estão em uso', 'value': '5 kWh/mês'},
     {'task': 'Usar lâmpadas de baixo consumo', 'value': '15 kWh/mês'},
@@ -11,6 +16,8 @@ class TodoListScreen extends StatelessWidget {
     {'task': 'Lavar roupas com água fria', 'value': '20 kWh/mês'},
     {'task': 'Usar um termostato programável', 'value': '25 kWh/mês'},
   ];
+
+  final List<bool> _completed = List<bool>.filled(6, false);
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +29,22 @@ class TodoListScreen extends StatelessWidget {
         itemCount: tasks.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(tasks[index]['task']!),
+            title: Text(
+              tasks[index]['task']!,
+              style: TextStyle(
+                decoration:
+                    _completed[index] ? TextDecoration.lineThrough : null,
+              ),
+            ),
             subtitle: Text('Economiza: ${tasks[index]['value']}'),
+            trailing: Checkbox(
+              value: _completed[index],
+              onChanged: (bool? value) {
+                setState(() {
+                  _completed[index] = value ?? false;
+                });
+              },
+            ),
           );
         },
       ),
